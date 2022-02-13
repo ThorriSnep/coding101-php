ce3fa963ac429de81e9f6d49ede7ecc82034bc94
Thorri <ThorriSnep@pm.me>
2022-02-13 22:38:59 +0100

    next challange done

---
+++ lvl1/messageotd.php
@@ -0,0 +1,22 @@
+<?php
+$arr = array(
+    "No one is perfect - that’s why pencils have erasers. - Wolfgang Riebe",
+    "Have no fear of perfection - you'll never reach it. - Salvador Dali",
+    "The tallest mountain started as a stone. - One Punch Man Intro",
+    "Make it work. Make it nice. Make it fast. Always obey this order! - kiraa",
+    "A good programmer is someone who always looks both ways before crossing a one-way street. – Doug Linder",
+    "If debugging is the process of removing software bugs, then programming must be the process of putting them in. - Edsger W. Dijkstra"
+);
+
+while (1) {
+    $rarr = range(0, 5);
+    shuffle($rarr);
+    for ($x = 0; count($rarr) > 0; $x++){
+        echo "\n";
+        echo $arr[$rarr[$x]];
+        unset($rarr[$x]);
+        echo "\n";
+    $a = readline('Enter: ');
+    }
+}
+?>
