0ac389d9404e68b7413c813def2f81792650d831
Thorri <ThorriSnep@pm.me>
2022-02-24 20:28:09 +0100

    completed cpu player for word guess

--- lvl2/wordguess.php
+++ lvl2/wordguess.php
@@ -3,7 +3,7 @@
 class CpuPlayer {
     public $dificulty;
     private $wordArray;
-    private $gameWords;
+    public $wordToGuess;
 
     public function setDificulty($dificulty) {
         /*  1) Easy (3-5)
@@ -14,7 +14,7 @@ class CpuPlayer {
     }
 
     public function getWords($wordFilePath) {
-        $fileContent = file_get_contents("LICENSE");
+        $fileContent = file_get_contents($wordFilePath);
 
         $charsToRm = ["(C)", "\n", "\r", ",", ".", "-", "<", ">", "´", "'", "`", "(", ")", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "\"", ";"];
         $fileContent = str_replace($charsToRm, " ", $fileContent);
@@ -53,14 +53,47 @@ class CpuPlayer {
         }
         $returnArr = [];
         for ($i = 0; $i < sizeof($wordArr); $i++) {
-            if (strlen($wordArr[$i]) <= $wordMax || strlen($wordArr[$i]) >= $wordMin) {
+            if (strlen($wordArr[$i]) <= $wordMax && strlen($wordArr[$i]) >= $wordMin) {
                 array_push($returnArr, $wordArr[$i]);
             }
         }
         if (sizeof($returnArr) == 0) {
             exit("Something went wrong\n");
         } else {
-            $this->$gameWords = $returnArr;
+            return $returnArr;
+        }
+    }
+
+    public function outputHint() {
+        $gameWords = $this->getDificultyWords($this->wordArray, $this->dificulty);
+        shuffle($gameWords);
+        $this->wordToGuess = $gameWords[0];
+        $firstLetter = $gameWords[0][0];
+        $lastLetter = $gameWords[0][strlen($gameWords[0]) - 1];
+        $lengthString = strlen($gameWords[0]);
+        print "first letter: $firstLetter, last letter:  $lastLetter, lenth string: $lengthString\n";   
+    }
+
+    public function inputGuess() {
+        $guessAmount = 0;
+        $running = true;
+        while($running) {
+            if($guessAmount == 3) {
+                exit("You reched the max ammount of guesses. Plese try again. It was: ".$this->wordToGuess."\n");
+            }
+            $userGuess = readline("Input guess: ");
+            if($userGuess == $this->wordToGuess) {
+                exit("Congrats you got it!\n");
+            } else {
+                print "Wrong!\n";
+                $guessAmount++;
+            }
         }
     }
 }
+
+$cpuPlayer1 = new CpuPlayer();
+$cpuPlayer1->getWords("LICENSE");
+$cpuPlayer1->setDificulty(1);
+$cpuPlayer1->outputHint();
+$cpuPlayer1->inputGuess();
