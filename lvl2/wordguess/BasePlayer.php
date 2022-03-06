e42eaa20b9f902d51bd7138a97d84f162f848ceb
Thorri <ThorriSnep@pm.me>
2022-03-06 12:25:33 +0100

    added BasePlayer Class

---
+++ lvl2/wordguess/BasePlayer.php
@@ -0,0 +1,29 @@
+<?php
+
+class BasePlayer {
+    public function outputHint() {
+        $wordToGuess = $this->wordToGuess ;
+        $firstLetter = $wordToGuess[0];
+        $lastLetter = $wordToGuess[strlen($wordToGuess) - 1];
+        $lengthString = strlen($wordToGuess);
+        print "first letter: $firstLetter, last letter:  $lastLetter, lenth string: $lengthString\n";   
+    }
+
+    public function inputGuess() {
+        $guessAmount = 0;
+
+        $running = true;
+        while ($running) {
+            if ($guessAmount == 3) {
+                exit("You reched the max ammount of guesses. Plese try again. It was: ".$this->wordToGuess."\n");
+            }
+            $userGuess = readline("Input guess: ");
+            if ($userGuess == $this->wordToGuess) {
+                exit("Congrats you got it!\n");
+            } else {
+                print "Wrong!\n";
+                $guessAmount++;
+            }
+        }
+    }
+}
